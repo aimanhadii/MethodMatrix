fc4e83ee48cc239e7cb06fd7a785f3787efc0f36
aiman <you@example.com>
2023-01-21 23:18:43 +0800

    refactor code into static method

--- src/java/main/assignment2/MethodMatrix.java
+++ src/java/main/assignment2/MethodMatrix.java
@@ -6,30 +6,24 @@ import java.io.Writer;
 
 public class MethodMatrix {
 
-    private int [][] A;
-    private int [][] B ;
 
     public static void main(String[] args) throws IOException {
-        MethodMatrix methodMatrix = new MethodMatrix();
-        methodMatrix.result(methodMatrix.multiMatrix());
-        methodMatrix.writeFile(methodMatrix.multiMatrix());
+        int[][] A = new int[][]{{-9, 1, 3}, {14, -5, 7}};
+        int[][] B = new int[][]{{1, 2}, {3, 4}, {5, 6}};
+        int[][] C = new int[2][2];
+
+        C = multiMatrix(A, B);
+        result(C);
+        writeFile(C);
     }
 
-    public MethodMatrix() {
-        this.A = new int[][]{{-9,1,3},{14,-5,7}};
-        this.B = new int[][] {{1,2},{3,4},{5,6}};
-    }
-
-    public int[][] multiMatrix() {
-        int [][]C = new int[2][2];
+    public static int[][] multiMatrix(int[][] A, int[][] B) {
+        int[][] C = new int[2][2];
 
         //multiply matrices A and B and store the matrix result in C
-        for(int i =0; i< 2; i++)
-        {
-            for(int j =0; j<2;j++)
-            {
-                for(int k=0; k<3; k++)
-                {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 3; k++) {
                     C[i][j] += A[i][k] * B[k][j];
                 }
             }
@@ -37,27 +31,27 @@ public class MethodMatrix {
         return C;
     }
 
-    public void result(int[][] C){
-        for(int i =0; i<2; i++)
-        {
-            for(int j=0;j<2;j++)
-            {
-                System.out.print(C[i][j]+ " ");
+    public static void result(int[][] C) {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(C[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public void writeFile(int[][] C) throws IOException {
-        Writer wr = new FileWriter("matrix.txt");
-        for(int i =0; i<2; i++)
-        {
-            for(int j=0;j<2;j++)
-            {
-                wr.write(C[i][j]+ " ");
+    public static void writeFile(int[][] C) throws IOException {
+        Writer wr = null;
+
+        wr = new FileWriter("matrix.txt");
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                wr.write(C[i][j] + " ");
             }
             wr.write("\n");
         }
         wr.close();
+
     }
 }
